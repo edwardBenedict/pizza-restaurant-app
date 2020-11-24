d6022c5e20f65e16d902650ee765fcc727961b24
Edward <edwardbenedict92@gmail.com>
2020-11-24 11:26:09 +0300

    third commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { GlobalStyle } from "./globalStyles";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import { productData, productDataTwo } from "./components/Products/data";
+import Feature from "./components/Feature";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Hero />
+      <Products heading="Choose your favourite" data={productData} />
+      <Feature />
+      <Products heading="Sweet Treats for You" data={productDataTwo} />
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
+
+// https://www.youtube.com/watch?v=GlROncAX4XI
+// Build a React Website Using Styled Components - Responsive Ecommerce Restaurant Design
---
+++ src/components/Products/index.js
@@ -0,0 +1,36 @@
+import {
+  ProductsContainer,
+  ProductImg,
+  ProductInfo,
+  ProductCard,
+  ProductDesc,
+  ProductPrice,
+  ProductTitle,
+  ProductsHeading,
+  ProductButton,
+  ProductWrapper,
+} from "./ProducsElements";
+const Products = ({ heading, data }) => {
+  return (
+    <ProductsContainer>
+      <ProductsHeading>{heading}</ProductsHeading>
+      <ProductWrapper>
+        {data.map((product, index) => {
+          return (
+            <ProductCard key={index}>
+              <ProductImg src={product.img} alt={product.alt} />
+              <ProductInfo>
+                <ProductTitle>{product.name}</ProductTitle>
+                <ProductDesc>{product.desc}</ProductDesc>
+                <ProductPrice>{product.price}</ProductPrice>
+                <ProductButton>{product.button}</ProductButton>
+              </ProductInfo>
+            </ProductCard>
+          );
+        })}
+      </ProductWrapper>
+    </ProductsContainer>
+  );
+};
+
+export default Products;
